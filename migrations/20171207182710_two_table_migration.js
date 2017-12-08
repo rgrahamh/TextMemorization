9c2ad181b4c8c0bb923e5ec8ce46e2f1063ba16b
Ryan Houck <ryan_houck@taylor.edu>
2017-12-07 21:39:02 -0500

    Forward and backward migrations are now functioning properly.

--- migrations/20171207182710_two_table_migration.js
+++ migrations/20171207182710_two_table_migration.js
@@ -2,22 +2,21 @@ exports.up = function(knex, Promise) {
     return knex.schema.dropTable('auth')
     .then(() => knex.schema.alterTable('users', users => {
         users.string('password');
-        users.integer('num_successful_attempts');
-        users.integer('num_unsuccessful_attempts');
+        users.integer('num_successful_login_attempts');
+        users.integer('num_unsuccessful_login_attempts');
     }));
 };
 
 exports.down = function(knex, Promise) {
     return knex.schema.createTable('auth', table => {
-        table.increments('login_id');
-        table.string('login_name');
+        table.string('login_name').primary();
         table.string('password');
         table.integer('num_successful_attempts');
         table.integer('num_unsuccessful_attempts');
     })
     .then(() => knex.schema.alterTable('users', users => {
         users.dropColumn('password');
-        users.dropColumn('num_successful_attempts');
-        users.dropColumn('num_unsuccessful_attempts');
+        users.dropColumn('num_successful_login_attempts');
+        users.dropColumn('num_unsuccessful_login_attempts');
     }));
 };
